6e416723f7deef6fa3485f125c34c9020a37a8cb
Terenteva Arina <arin.terentjeva@yandex.ru>
2022-02-03 08:22:11 +0300

    feat: настроить вебпак

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import "../pages/index.css";
 import { enableValidation } from "./validation.js";
 import { addCard } from "./card.js";
 import { editProfile, openPopup, updateInputValue} from "./modal.js";
